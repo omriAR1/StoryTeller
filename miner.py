ea38210e62431dc18c3c65b3f3b7ee706c9ea86b
omriAR1 <51694763+omriAR1@users.noreply.github.com>
2022-01-03 18:00:50 +0200

    Add files via upload
    Add plot information to dataset, using wikipedia

--- miner.py
+++ miner.py
@@ -9,14 +9,19 @@ wiki_wiki = wikipediaapi.Wikipedia('en')
 def search_book_description(book_title):
 	# if there is a space in book title, replace with underscore
 	if book_title.find(' ') != -1:
-		book_title = book_title.replace(' ', '_')
-	print(f"Book title after processing: {book_title}")
+		book_title_wiki = book_title.replace(' ', '_')
+		book_title_depo = book_title.replace(' ', '-')
+	print(f"Book title after processing:\nwiki: {book_title}\nBook Depo:{book_title_depo}")
 
 	wiki_result, wiki_text = wikipedia_search(book_title)
 	if wiki_result:
 		print(f"Found Title {book_title} in wiki search")
 		return wiki_text
 	else:
+		book_depo, book_depo_text = book_depo_search(book_title_depo)
+		if book_depo:
+			return book_depo_text
+
 		amaz_result = amazon_search(book_title)
 		if amaz_result:
 			return amaz_result
@@ -36,6 +41,9 @@ def wikipedia_search(book_title):
 	print(f"Page - Summary: {result.summary}")
 	return True, result.summary
 
+
+def book_depo_search(book_title):
+	req = requests.get(url=f"https://www.bookdepository.com/")
 
 
 def amazon_search(book_title):
